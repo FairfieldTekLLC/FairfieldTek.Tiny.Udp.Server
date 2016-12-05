42fddd6111a81ec2a7abbba2d4838f30ac0de311
Winterleaf <VGee@winterleafentertainment.com>
2016-12-04 21:53:32 -0500

    Copyright Notice

--- Examples/FairfieldTekLLC.Tiny.Udp.Example.Client.Mono/Datagram/Authenticate.cs
+++ Examples/FairfieldTekLLC.Tiny.Udp.Example.Client.Mono/Datagram/Authenticate.cs
@@ -1,15 +1,27 @@
-﻿// // Fairfield Tek L.L.C.
-// // Copyright (c) 2016, Fairfield Tek L.L.C.
-// // 
-// // 
-// // THIS SOFTWARE IS PROVIDED BY WINTERLEAF ENTERTAINMENT LLC ''AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
-// //  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
-// // PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL WINTERLEAF ENTERTAINMENT LLC BE LIABLE FOR ANY DIRECT, INDIRECT, 
-// // INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
-// // SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND 
-// // ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
-// // OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH 
-// // DAMAGE. 
+﻿//  Fairfield Tek L.L.C.
+//  Copyright (c) 2016, Fairfield Tek L.L.C.
+//  
+//  
+// THIS SOFTWARE IS PROVIDED BY WINTERLEAF ENTERTAINMENT LLC ''AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+// INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL WINTERLEAF ENTERTAINMENT LLC BE LIABLE FOR ANY DIRECT, INDIRECT, 
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND 
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
+// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH 
+// DAMAGE. 
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 using FairfieldTekLLC.Tiny.Udp.Server.Common.BaseClass;
 using FairfieldTekLLC.Tiny.Udp.Server.Common.Common;
@@ -17,10 +29,10 @@ using FairfieldTekLLC.Tiny.Udp.Server.Common.Common;
 namespace FairfieldTekLLC.Tiny.Udp.Example.Client.Mono.Datagram
 {
     /// <summary>
-    /// This Datagram is used to set the users handle on the server.
-    /// One sent, it will either respond with what was sent, meaning that
-    /// it was accepted or it will respond with empty string meaning
-    /// it was invalid.
+    ///     This Datagram is used to set the users handle on the server.
+    ///     One sent, it will either respond with what was sent, meaning that
+    ///     it was accepted or it will respond with empty string meaning
+    ///     it was invalid.
     /// </summary>
     internal class Authenticate : DatagramBase
     {
@@ -31,7 +43,7 @@ namespace FairfieldTekLLC.Tiny.Udp.Example.Client.Mono.Datagram
         public string Handle { get; set; } = string.Empty;
 
         /// <summary>
-        /// Packs our variables
+        ///     Packs our variables
         /// </summary>
         /// <param name="streamProcessor"></param>
         /// <returns></returns>
@@ -42,7 +54,7 @@ namespace FairfieldTekLLC.Tiny.Udp.Example.Client.Mono.Datagram
         }
 
         /// <summary>
-        /// Unpacks our variables.
+        ///     Unpacks our variables.
         /// </summary>
         /// <param name="streamProcessor"></param>
         /// <returns></returns>
--- README.md
+++ README.md
@@ -1,5 +1,26 @@
 # FairfieldTek.Tiny.Udp.Server
 
-This project implements a fully functional Udp Sockets Server.
+This project implements a fully functional Udp Sockets Server with examples of how to use it.
+The software is owned and copyrighted by Fairfield Tek L.L.C. (http://www.FairfieldTek.com)
 
 ## License ##
+Copyright [2016] Fairfield Tek L.L.C.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+ Fairfield Tek L.L.C.
+ Copyright (c) 2016, Fairfield Tek L.L.C.
+ 
+ 
+ THIS SOFTWARE IS PROVIDED BY WINTERLEAF ENTERTAINMENT LLC ''AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
+ PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL WINTERLEAF ENTERTAINMENT LLC BE LIABLE FOR ANY DIRECT, INDIRECT, 
+ INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+ SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND 
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
+ OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH 
+ DAMAGE. 
